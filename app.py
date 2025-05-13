43d65e1d659dee71a085629985f3b39e9aeffa7a
Joseph Bryan Ferrer <qjbferrer@tip.edu.ph>
2025-05-14 02:29:22 +0800

    Initial commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Define constants
-MODEL_PATH = "server/models/inceptionv3.pth"
+MODEL_PATH = "models/inceptionv3.pth"
 NUM_CLASSES = 3
 CLASS_NAMES = ["Armyworm", "Cutworm", "Red_Spider_Mites"]
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
